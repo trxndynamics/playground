56ea8dd0b9cf8d55c3b7a812f0c21d7ea9ca045b
Martin Evans <Martin0123456@hotmail.com>
2014-08-30 00:21:11 +0100

    Addition of Rockmongo to the VM.  Simply add a rockmongo.playground.vm to your configuration file and it should work.
    CSS update for styling changes made to the first time start.
    
    Updated composer.json to allow for phpunit testing.

--- server/shared/playground/laravel/app/tests/ExampleTest.php
+++ server/shared/playground/laravel/app/tests/ExampleTest.php
@@ -14,4 +14,15 @@ class ExampleTest extends TestCase {
 		$this->assertTrue($this->client->getResponse()->isOk());
 	}
 
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testAnotherExample()
+    {
+        $crawler = $this->client->request('GET', '/');
+
+        $this->assertTrue($this->client->getResponse()->isOk());
+    }
 }
