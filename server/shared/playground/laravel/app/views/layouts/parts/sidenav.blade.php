e8428b9f8a764f71c0e1e8510722342dc0804345
Martin Evans <martin.evans@phpcoder.guru>
2014-09-03 23:23:06 +0100

    Updated Side Nav

--- server/shared/playground/laravel/app/views/layouts/parts/sidenav.blade.php
+++ server/shared/playground/laravel/app/views/layouts/parts/sidenav.blade.php
@@ -1,12 +1,9 @@
 <ul id="active" class="nav navbar-nav side-nav">
     <li class="selected"><a href="index.html"><i class="fa fa-bullseye"></i> Dashboard</a></li>
-    <li><a href="portfolio.html"><i class="fa fa-tasks"></i> Portfolio</a></li>
-    <li><a href="blog.html"><i class="fa fa-globe"></i> Blog</a></li>
-    <li><a href="signup.html"><i class="fa fa-list-ol"></i> SignUp</a></li>
-    <li><a href="register.html"><i class="fa fa-font"></i> Register</a></li>
-    <li><a href="timeline.html"><i class="fa fa-font"></i> Timeline</a></li>
-    <li><a href="forms.html"><i class="fa fa-list-ol"></i> Forms</a></li>
-    <li><a href="typography.html"><i class="fa fa-font"></i> Typography</a></li>
-    <li><a href="bootstrap-elements.html"><i class="fa fa-list-ul"></i> Bootstrap Elements</a></li>
-    <li><a href="bootstrap-grid.html"><i class="fa fa-table"></i> Bootstrap Grid</a></li>
+    <li><a href="/league/calendar"><i class="fa fa-tasks"></i> Fixtures</a></li>
+    <li><a href="/player/search"><i class="fa fa-globe"></i> Scouting</a></li>
+    <li><a href="/highlights"><i class="fa fa-list-ol"></i> Highlights</a></li>
+    <li><a href="/player/stats"><i class="fa fa-font"></i> Player Stats</a></li>
+    <li><a href="/league/table"><i class="fa fa-font"></i> League Table</a></li>
+    <li><a href="/squad/fitness"><i class="fa fa-list-ol"></i> Injury Table</a></li>
 </ul>
