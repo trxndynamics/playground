4597bc3977826ecefb8da53587393b35b44dd7c3
Martin Evans <martin.evans@phpcoder.guru>
2014-09-10 23:21:38 +0100

    Basic Models

---
+++ server/shared/playground/laravel/app/models/League.php
@@ -0,0 +1,18 @@
+<?php
+
+class League extends Moloquent {
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'leagues';
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = array();
+
+}
---
+++ server/shared/playground/laravel/app/models/Team.php
@@ -0,0 +1,18 @@
+<?php
+
+class Team extends Moloquent {
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'teams';
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = array();
+
+}
