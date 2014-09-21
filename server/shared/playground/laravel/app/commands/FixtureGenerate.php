bb007e6d700655037e896d1969987c0db78a9113
Martin Evans <martin.evans@phpcoder.guru>
2014-09-21 19:02:50 +0100

    Minimal update , start date + number of weeks added to fixture generator.

--- server/shared/playground/laravel/app/commands/FixtureGenerate.php
+++ server/shared/playground/laravel/app/commands/FixtureGenerate.php
@@ -37,22 +37,45 @@ class FixtureGenerate extends Command {
 	 */
 	public function fire()
 	{
+        $startDateTimestamp = strtotime('second sat of august '.date('Y'));
+        $endDateTimestamp   = strtotime('first sat of may '.date('Y'));
+
         //get the start date and end date of football league calendar
-        $startDate  = strtotime('second sat of august '.date('Y'));    //start date
-        $endDate    = strtotime('first sat of may '.date('Y'));        //end date
+        $startDate  = \Carbon\Carbon::createFromTimestamp($startDateTimestamp);    //start date
+        $endDate    = \Carbon\Carbon::createFromTimestamp($endDateTimestamp);        //end date
+
+        //get the number of saturdays between those two periods
+        $numberOfSaturdays = intval(floor(($startDate->diffInDaysFiltered(function(\Carbon\Carbon $date){
+            return $date->isWeekend();
+        }, $endDate))/2));
 
         //get the number of teams for a particular league
         $leagueName = 'Bundesliga';
 
+        //get the number of weeks
         $league = League::where('name','=',$leagueName)->first();
         $teams  = Team::where('league','=',$leagueName)->get();
 
         //count the number of teams
-        $numberOfTeams = count($teams);
+        $numberOfTeams  = count($teams);
+        $homeFixtures   = [];
 
         foreach($teams as $team){
-            var_dump($team->name);
+            $teamList[$team->id] = $team->name;
         }
+
+        //get the number of fixtures for each team
+        $numberOfFixtureWeeksForEachTeam = ($numberOfTeams-1)*2;
+
+        for($i=0; $i < $numberOfFixtureWeeksForEachTeam; $i++){
+            $fixtureDate = \Carbon\Carbon::createFromTimestamp($startDateTimestamp);
+            $fixtureDate->addWeeks($i);
+
+
+        }
+
+        var_dump($numberOfTeams / $numberOfSaturdays);
+
 	}
 
 }
