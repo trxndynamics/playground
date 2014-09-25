a3962cb66cf688365bbdce3efe064c69c3931962
Martin Evans <martin.evans@phpcoder.guru>
2014-09-25 22:16:54 +0100

    sample match added for reference purposes

---
+++ server/shared/playground/data/sample/match.js
@@ -0,0 +1,14 @@
+db.getCollection("leagues").insert({
+    "date": {
+    "date": "2014-09-24 00:00:00.000000",
+        "timezone_type": NumberInt(3),
+        "timezone": "UTC"
+},
+    "dateTimestamp": NumberInt(1411516800),
+    "userId": "53fd0feb10f0ed20048b4567",
+    "location": "away",
+    "home": "Mars Maestros",
+    "away": "Bayer 04 Leverkusen",
+    "updated_at": ISODate("2014-09-23T19:55:10.315Z"),
+    "created_at": ISODate("2014-09-23T19:55:10.315Z")
+});
