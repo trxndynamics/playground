08d1047375591d4b27c737f63feae6f4731cec58
Martin Evans <martin.evans@phpcoder.guru>
2014-09-26 23:37:47 +0100

    Initial fixture generation grid added.

--- server/shared/playground/laravel/app/commands/FixtureGenerate.php
+++ server/shared/playground/laravel/app/commands/FixtureGenerate.php
@@ -6,81 +6,121 @@ use Symfony\Component\Console\Input\InputArgument;
 
 class FixtureGenerate extends Command {
 
-	/**
-	 * The console command name.
-	 *
-	 * @var string
-	 */
-	protected $name = 'FixtureGenerate';
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'FixtureGenerate';
 
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
-	protected $description = 'Command description.';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description.';
 
-	/**
-	 * Create a new command instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		parent::__construct();
-	}
 
-	/**
-	 * Execute the console command.
-	 *
-	 * @return mixed
-	 */
-	public function fire()
-	{
+    /**
+     * Generates the fixtures
+     *
+     * @param int $quantityOfTeams
+     */
+    private function getFixtures($quantityOfTeams){
+        $fixtures = [];
+
+        for($i=0;$i<$quantityOfTeams; $i++){
+            for($j=0;$j<$quantityOfTeams;$j++){
+                if($j==$i)  continue;
+
+                $fixtures[$i][] = $j;
+            }
+        }
+
+        return $fixtures;
+    }
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    private function getFixture($remainingFixturesForATeam){
+
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function fire()
+    {
         $startDateTimestamp = strtotime('second sat of august '.date('Y'));
-        $endDateTimestamp   = strtotime('first sat of may '.date('Y'));
+//        $endDateTimestamp   = strtotime('first sat of may '.date('Y'));
+
+        $leagueName = 'Bundesliga';
+        $league     = League::where('name','=',$leagueName)->first();
+        $teamList   = Team::where('league','=',$leagueName)->get();
 
         //get the start date and end date of football league calendar
         $startDate  = \Carbon\Carbon::createFromTimestamp($startDateTimestamp);    //start date
-        $endDate    = \Carbon\Carbon::createFromTimestamp($endDateTimestamp);        //end date
+//        $endDate    = \Carbon\Carbon::createFromTimestamp($endDateTimestamp);        //end date
 
-        //get the number of saturdays between those two periods
-        $numberOfSaturdays = intval(floor(($startDate->diffInDaysFiltered(function(\Carbon\Carbon $date){
-            return $date->isWeekend();
-        }, $endDate))/2));
+        $games = array();
+        $teams = count($teamList);
 
-        //get the number of teams for a particular league
-        $leagueName = 'Bundesliga';
+        $arr    = ['bolton','man utd', 'leeds', 'wigan'];
+        $teams  = count($arr);
 
-        //get the number of weeks
-        $league = League::where('name','=',$leagueName)->first();
-        $teams  = Team::where('league','=',$leagueName)->get();
-
-        //count the number of teams
-        $numberOfTeams  = count($teams);
-        $homeFixtures   = [];
-        $awayFixtures   = [];
-        $teamList       = [];
-
-        foreach($teams as $team){
-            $teamList[$team->id] = $team->name;
+        // do the work
+        for( $i=1; $i<=$teams; $i++ ) {
+            $games[$i] = array();
+            for( $j=1; $j<=$teams; $j++ ) {
+                $games[$i][$j] = $this->getWeek($i, $j, $teams);
+            }
         }
 
-        //get the number of fixtures for each team
-        $numberOfFixtureWeeksForEachTeam = ($numberOfTeams-1)*2;
 
-        for($i=0; $i < $numberOfFixtureWeeksForEachTeam; $i++){
-            $lastWeeksHome  = [];
-            $fixtureDate    = \Carbon\Carbon::createFromTimestamp($startDateTimestamp)->addWeeks($i);
-            $thisWeeksTeams = $teamList;
+        // display
+        $max=0;
+        foreach($games as $row) {
+            foreach($row as $col) {
+                printf('%4d', is_null($col) ? -2 : $col);
+                if( $col > $max ) { $max=$col; }
+            }
+            echo "\n";
+        }
+        printf("%d teams in %d weeks, %.2f weeks per team\n", $teams, $max, $max/$teams);
 
+        for($i=1; $i<=count($games); $i++){
+            for($j=1; $j<=count($games); $j++){
+                if($i==$j) continue;
 
+                $fixtureDate = $this->getWeek($i, $j, $teams);
 
-            exit();
+                var_dump($arr[$i-1].' - '.$arr[$j-1].' = '.$this->getWeek($i, $j, $teams));
+            }
+        }
+    }
+
+    function getWeek($home, $away, $num_teams) {
+        if($home == $away){
+            return -1;
+        }
+        $week = $home+$away-2;
+        if($week >= $num_teams){
+            $week = $week-$num_teams+1;
+        }
+        if($home>$away){
+            $week += $num_teams-1;
         }
 
-        var_dump($numberOfTeams / $numberOfSaturdays);
-
-	}
-
+        return $week;
+    }
 }
