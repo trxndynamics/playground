e2c65519b767d99d7cac182eee09d232793720d6
Martin Evans <martin.evans@phpcoder.guru>
2014-09-28 00:44:03 +0100

    Upgrade to javascript to hide players that have already been selected.  Speed improvements also to the JS to prevent JS from fetching the element on multiple occassions

--- server/shared/playground/laravel/public/resource/js/select-player.js
+++ server/shared/playground/laravel/public/resource/js/select-player.js
@@ -1,18 +1,22 @@
 $(document).ready(function(){
+    var playerList = $('#playerList');
+
     $('.playerSelect').on('click', function(){
-        var playerCount = $('#playerList > div').length;
+        var playerCount = playerList.children('div').length;
 
         if(playerCount < 11){
-            if($('#playerList > #pid_'+$(this).data('id')).length < 1){
-                $('#playerList').append(
+            $(this).closest('tr').addClass('hidden');
+
+            if(playerList.children('#pid_'+$(this).data('id')).length < 1){
+                playerList.append(
                     '<div id="pid_' + $(this).data('id') + '">' +
-                        '<span style="font-weight:bold">' + $(this).data('position') + '</span>   ' +
-                        $(this).data('name') + ' ' +
-                        '</div><input type="hidden" name="player[]" value="'+$(this).data('id')+'" />'
+                    '<span style="font-weight:bold">' + $(this).data('position') + '</span>   ' +
+                    $(this).data('name') + ' ' +
+                    '</div><input type="hidden" name="player[]" value="'+$(this).data('id')+'" />'
                 );
 
-                if($('#playerList > div').length == 11){
-                    $('#playerList').append('<input type="submit">');
+                if(playerList.children('div').length == 11){
+                    playerList.append('<input type="submit">');
                 }
             } else {
                 alert('the same player cannot be added twice');
