3b246fce5b0661c49dd3f5c2bc34422956796585
Martin Evans <martin.evans@phpcoder.guru>
2014-11-12 20:06:13 +0000

    Added secondary team and ypos offset

--- server/shared/playground/laravel/app/views/display/pages/test/phaser.blade.php
+++ server/shared/playground/laravel/app/views/display/pages/test/phaser.blade.php
@@ -12,17 +12,29 @@
 <?php
 
 $players = [
-    't1p1' => array('src'=>'/resource/images/player/faces/2.png', 'xpos' => 70, 'ypos' => 0),
-    't1p2' => array('src'=>'/resource/images/player/faces/16.png', 'xpos' => 140, 'ypos' => 0),
-    't1p3' => array('src'=>'/resource/images/player/faces/27.png', 'xpos' => 210, 'ypos' => 0),
-    't1p4' => array('src'=>'/resource/images/player/faces/41.png', 'xpos' => 280, 'ypos' => 0),
-    't1p5' => array('src'=>'/resource/images/player/faces/58.png', 'xpos' => 350, 'ypos' => 0),
-    't1p6' => array('src'=>'/resource/images/player/faces/80.png', 'xpos' => 420, 'ypos' => 0),
-    't1p7' => array('src'=>'/resource/images/player/faces/164.png', 'xpos' => 490, 'ypos' => 0),
-    't1p8' => array('src'=>'/resource/images/player/faces/201.png', 'xpos' => 560, 'ypos' => 0),
-    't1p9' => array('src'=>'/resource/images/player/faces/241.png', 'xpos' => 630, 'ypos' => 0),
-    't1p10' => array('src'=>'/resource/images/player/faces/250.png', 'xpos' => 700, 'ypos' => 0),
-    't1p11' => array('src'=>'/resource/images/player/faces/11.png', 'xpos' => 770, 'ypos' => 0)
+    't1p1' => array('src'=>'/resource/images/player/faces/2.png', 'xpos' => 70, 'ypos' => 100),
+    't1p2' => array('src'=>'/resource/images/player/faces/16.png', 'xpos' => 140, 'ypos' => 100),
+    't1p3' => array('src'=>'/resource/images/player/faces/27.png', 'xpos' => 210, 'ypos' => 100),
+    't1p4' => array('src'=>'/resource/images/player/faces/41.png', 'xpos' => 280, 'ypos' => 100),
+    't1p5' => array('src'=>'/resource/images/player/faces/58.png', 'xpos' => 350, 'ypos' => 100),
+    't1p6' => array('src'=>'/resource/images/player/faces/80.png', 'xpos' => 420, 'ypos' => 100),
+    't1p7' => array('src'=>'/resource/images/player/faces/164.png', 'xpos' => 490, 'ypos' => 100),
+    't1p8' => array('src'=>'/resource/images/player/faces/201.png', 'xpos' => 560, 'ypos' => 100),
+    't1p9' => array('src'=>'/resource/images/player/faces/241.png', 'xpos' => 630, 'ypos' => 100),
+    't1p10' => array('src'=>'/resource/images/player/faces/250.png', 'xpos' => 700, 'ypos' => 100),
+    't1p11' => array('src'=>'/resource/images/player/faces/11.png', 'xpos' => 770, 'ypos' => 100),
+
+    't2p1' => array('src'=>'/resource/images/player/faces/2.png', 'xpos' => 70, 'ypos' => 350),
+    't2p2' => array('src'=>'/resource/images/player/faces/16.png', 'xpos' => 140, 'ypos' => 350),
+    't2p3' => array('src'=>'/resource/images/player/faces/27.png', 'xpos' => 210, 'ypos' => 350),
+    't2p4' => array('src'=>'/resource/images/player/faces/41.png', 'xpos' => 280, 'ypos' => 350),
+    't2p5' => array('src'=>'/resource/images/player/faces/58.png', 'xpos' => 350, 'ypos' => 350),
+    't2p6' => array('src'=>'/resource/images/player/faces/80.png', 'xpos' => 420, 'ypos' => 350),
+    't2p7' => array('src'=>'/resource/images/player/faces/164.png', 'xpos' => 490, 'ypos' => 350),
+    't2p8' => array('src'=>'/resource/images/player/faces/201.png', 'xpos' => 560, 'ypos' => 350),
+    't2p9' => array('src'=>'/resource/images/player/faces/241.png', 'xpos' => 630, 'ypos' => 350),
+    't2p10' => array('src'=>'/resource/images/player/faces/250.png', 'xpos' => 700, 'ypos' => 350),
+    't2p11' => array('src'=>'/resource/images/player/faces/11.png', 'xpos' => 770, 'ypos' => 350)
 ];
 
 ?>
@@ -53,7 +65,7 @@ $players = [
                         $offset += 70;
                     ?>
 
-                    var <?php echo $ref;?> = game.add.sprite(<?php echo $player['xpos'] ?>, game.world.centerY, '<?php echo $ref; ?>');
+                    var <?php echo $ref;?> = game.add.sprite(<?php echo $player['xpos'] ?>, <?php echo $player['ypos'] ?>, '<?php echo $ref; ?>');
                     <?php echo $ref; ?>.anchor.setTo(0.5, 0.5);
 
                     <?php
